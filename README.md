bd0e5415b05ff02995c38ce11c175fd30cdf4ee5
supersanta183 <klemmensen11@gmail.com>
2024-04-11 15:42:47 +0200

    added ability to generate seedphrase and generate keypair from seedphrase

--- README.md
+++ README.md
@@ -9,9 +9,16 @@ Cargo install rsa_keygen_cli
 current options:
 --help: displays a list of options
 --new: generates a 12 word seedphrase and associating rsa keypair in pksc8 pem format.
+--fromseed: generates rsa keypair from already known seedphrase
+--newseed: generates a new seedphrase without rsa keypair
 
 Example usage:
-´´´Rust
+´´´
 rsakeygen --new
 ´´´
+
+generating keypair from seed:
+```
+rsakeygen --fromseed "brother above used lounge afraid cash half universe spatial casual notable hope"
+```
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::{Command, Parser};
-use rsa_keygen::{ generate_seedphrase_and_keypair, pkcs8_pem_from_priv_key, pkcs8_pem_from_pub_key };
+use rsa_keygen::{ generate_seedphrase_and_keypair, pkcs8_pem_from_priv_key, pkcs8_pem_from_pub_key, keypair_from_seedphrase, generate_seedphrase };
+use rsa::{RsaPrivateKey, RsaPublicKey};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -7,6 +8,14 @@ struct AppArgs {
     /// generate seedphrase and associated keypair
     #[clap(short, long)]
     new: bool,
+
+    /// generate keypair from seedphrase
+    #[clap(short, long)]
+    fromseed: Option<String>,
+
+    /// generate new seedphrase
+    #[clap(short, long)]
+    newseed: bool
 }
 
 fn main() {
@@ -15,11 +24,17 @@ fn main() {
     if args.new {
         println!("Generating new seedphrase and keypair \n");
         display_seedphrase_and_keypair();
+    } else if let Some(seedphrase) = args.fromseed {
+        println!("Generating keypair from seedphrase {}", seedphrase);
+        derive_keypair_from_seedphrase(&seedphrase);
+    } else if args.newseed {
+        println!("Generating seedphrase");
+        gen_seedphrase();
     }
 }
 
 fn display_seedphrase_and_keypair() {
-    let (seedphrase, (priv_key, pub_key)) = match generate_seedphrase_and_keypair() {
+    let (seedphrase, keypair) = match generate_seedphrase_and_keypair() {
         Ok((seedphrase, keypair)) => (seedphrase, keypair),
         Err(e) => {
             eprintln!("Error: {}", e);
@@ -27,6 +42,23 @@ fn display_seedphrase_and_keypair() {
         }
     };
     println!("Seedphrase: {} \n", seedphrase);
+    print_keypair(keypair);
+}
+
+fn derive_keypair_from_seedphrase(seedphrase: &String) {
+    println!("hej");
+    println!("{}", seedphrase);
+    let keypair = keypair_from_seedphrase(seedphrase).expect("failed to generate keypair from seedphrase");
+    print_keypair(keypair);
+}
+
+fn gen_seedphrase() {
+    let seedphrase = generate_seedphrase();
+    println!("seedphrase: {}", seedphrase);
+}
+
+fn print_keypair(keypair: (RsaPrivateKey, RsaPublicKey)) {
+    let (priv_key, pub_key) = keypair;
     let pem_pub_key = pkcs8_pem_from_pub_key(&pub_key).unwrap();
     let pem_priv_key = pkcs8_pem_from_priv_key(&priv_key).unwrap();
     println!("{}", pem_priv_key.as_str());
